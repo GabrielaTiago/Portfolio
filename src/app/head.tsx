387adb8b9c597e26aeb20cd84b429e365a67f458
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-20 18:09:07 -0300

    :tada: init: Initial commit

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Gabriela Tiago Portfolio</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Portfolio web developer fullstack, frontend, backend" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="text-5xl font-bold">Bem Vindo(a)!!</main>;
+}
