5c57dbca92def2aa661c234ddc148c9d4c1d5c57
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-21 18:01:17 -0300

    :lipstick: feat: Changes margin size

--- src/components/BackgroundCircles.tsx
+++ src/components/BackgroundCircles.tsx
@@ -13,11 +13,11 @@ export function BackgroundCircles() {
       transition={{ duration: 2.0 }}
       className="relative flex justify-center items-center"
     >
-      <div className="absolute border border-[#333333] rounded-full w-[200px] h-[200px] mt-32 animate-ping" />
-      <div className="absolute border border-[#333333] rounded-full w-[300px] h-[300px] mt-32  opacity-80" />
-      <div className="absolute border border-[#333333] rounded-full w-[500px] h-[500px] mt-32" />
-      <div className="absolute border border-[#34ebcd] rounded-full w-[650px] h-[650px] mt-32 animate-pulse opacity-20" />
-      <div className="absolute border border-[#333333] rounded-full w-[800px] h-[800px] mt-32" />
+      <div className="absolute border border-[#333333] rounded-full w-[200px] h-[200px] mt-52 animate-ping" />
+      <div className="absolute border border-[#333333] rounded-full w-[300px] h-[300px] mt-52  opacity-80" />
+      <div className="absolute border border-[#333333] rounded-full w-[500px] h-[500px] mt-52" />
+      <div className="absolute border border-[#34ebcd] rounded-full w-[650px] h-[650px] mt-52 animate-pulse opacity-20" />
+      <div className="absolute border border-[#333333] rounded-full w-[800px] h-[800px] mt-52" />
     </motion.div>
   );
 }
