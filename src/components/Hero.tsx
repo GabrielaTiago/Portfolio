c61821e6f81ca2f058e146c98390e94ce6414d34
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-21 18:11:13 -0300

    :sparkles: feat: Creates the 'Picture' component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
 
-import { BackgroundCircles,  Typewriter } from "../components";
+import { BackgroundCircles, Picture, Typewriter } from "../components";
 
 export function Hero() {
   return (
     <div className="h-screen flex flex-col items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-
+      <Picture />
       <div className="flex flex-col mt-4 gap-2 z-20">
         <h2 className="uppercase text-sm text-gray-500 tracking-[11px]">
           Desenvolvedora Web Fullstack JR
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./BackgroundCircles";
 export * from "./Header";
 export * from "./Hero";
+export * from "./Picture";
 export * from "./Typewriter";
