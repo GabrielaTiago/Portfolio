2ae5e41cf64e534880ad49f793b8a47f6b1479f8
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-22 13:40:45 -0300

    :sparkles: feat: Creates the 'About' component

---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+export function About() {
+  return (
+    <div className="relative h-screen px-10 mx-auto flex flex-col items-center justify-evenly text-center md:text-left md:flex-row">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Sobre
+      </h3>
+    </div>
+  );
+}
