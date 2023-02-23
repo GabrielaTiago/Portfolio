8e995732bd8aa4e1846bc1af298685be5c61f9d4
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-23 10:29:09 -0300

    :sparkles: feat: Creates the 'AboutPicture' component

---
+++ src/components/AboutPicture.tsx
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+
+export function AboutPicture() {
+  return (
+    <motion.img
+      initial={{ x: -200, opacity: 0 }}
+      transition={{ duration: 1.2 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      viewport={{ once: true }}
+      src="https://avatars.githubusercontent.com/u/77814822?v=4"
+      className="-mb-20 md:mb-0 w-56 h-56 flex-shrink-0 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+    />
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export * from "./About";
+export * from "./AboutPicture";
 export * from "./BackgroundCircles";
 export * from "./Header";
 export * from "./Hero";
