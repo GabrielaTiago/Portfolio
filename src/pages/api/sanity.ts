7db86d8193dce20898dc879bb1e777730bbae0bf
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-27 18:36:27 -0300

    :sparkles: feat: Implements the 'snanity' into the project

---
+++ src/pages/api/sanity.ts
@@ -0,0 +1,22 @@
+import { createClient } from "next-sanity";
+import createImageBuilder from "@sanity/image-url";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "production";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!;
+const useCdn = process.env.NODE_ENV === "production";
+
+export const config = {
+  dataset,
+  projectId,
+  apiVersion,
+  useCdn,
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: any) => {
+    createImageBuilder(config).image(source);
+}
+
+
