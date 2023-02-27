14d07e6bc10050b9faea7a7d9074843fd970336b
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-27 18:31:03 -0300

    :sparkles: feat: Implements the snanity schemas

---
+++ portfolio-/schemas/skill.ts
@@ -0,0 +1,29 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'skill',
+  title: 'Skill',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      description: 'Title of Skill',
+      type: 'string',
+    }),
+    defineField({
+      name: 'time',
+      title: 'Time',
+      description: 'How long I started to use',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
