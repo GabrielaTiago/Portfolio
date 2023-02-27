bbcb0708785ccd7d3108bd46719c69fc661b06f9
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-27 18:43:06 -0300

    :sparkles: feat: Requests to the socials endpoint

---
+++ src/pages/api/getSocials.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { groq } from "next-sanity";
+import { sanityClient } from "./sanity";
+import { Social } from "../../Interfaces/sanityInterfaces";
+
+type SocialData = {
+  socials: Social[];
+};
+
+const query = groq`
+    *[_type == "social"]
+`;
+
+export default async function handlerSocials(
+  req: NextApiRequest,
+  res: NextApiResponse<SocialData>
+) {
+  const socials: Social[] = await sanityClient.fetch(query);
+
+  res.status(200).json({ socials });
+}
