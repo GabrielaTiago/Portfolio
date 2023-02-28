6ca00bffc4eb8098a32844817f5c9723b40b1df3
GabsTiago <gabrielatiagodearaujo@outlook.com>
2023-02-28 19:37:53 -0300

    :boom: fix: Missing parameter in sanity schemas

--- portfolio-/schemas/pageInfo.ts
+++ portfolio-/schemas/pageInfo.ts
@@ -26,7 +26,8 @@ export default defineType({
     defineField({
       name: 'backgroundInfo',
       title: 'BackgroundInfo',
-      type: 'string',
+      type: 'array',
+      of: [{type: 'string'}],
     }),
     defineField({
       name: 'profilePicture',
