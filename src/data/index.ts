5c42d8d549224fb93453197a0cfd1ef2ada20bcc
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-07-21 23:49:20 -0300

    :sparkles: feat: Add data for the grid

--- src/data/index.ts
+++ src/data/index.ts
@@ -4,3 +4,71 @@ export const navItems = [
 	{ name: 'Experiência', link: '#workexperience' },
 	{ name: 'Contato', link: '#contact' },
 ];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: 'Priorizo o contato com o cliente, promovendo a comunicação aberta',
+		description: '',
+		className: 'lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[55vh]',
+		imgClassName: 'w-full h-full',
+		titleClassName: 'justify-end',
+		img: '/b1.svg',
+		spareImg: '',
+	},
+	{
+		id: 2,
+		title: 'Sou flexível com comunicações em diferentes fusos',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-center',
+		img: '',
+		spareImg: '',
+	},
+	{
+		id: 3,
+		title: 'Minhas principais tecnologias',
+		description: 'Sempre me desenvolvendo',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-center lg:max-w-72',
+		img: '',
+		spareImg: '',
+	},
+	{
+		id: 4,
+		title: 'Entusiasta de tecnologia com paixão por desenvolvimento',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-center',
+		img: '/grid.svg',
+		spareImg: '/b4.svg',
+	},
+	{
+		id: 5,
+		title: 'Atualmente estou construindo um sistema para controle de manutenções veiculares',
+		description: 'Em primeira mão',
+		className: 'md:col-span-3 md:row-span-2',
+		imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
+		titleClassName: 'justify-center sm:max-w-72 lg:max-w-96',
+		img: '/b5.svg',
+		spareImg: '/grid.svg',
+	},
+	{
+		id: 6,
+		title: 'Vamos iniciar um projeto juntos?',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-center items-center max-w-full text-center',
+		img: '',
+		spareImg: '',
+	},
+];
+
+export const mainTechnologies = {
+	leftLists: ['ReactJS', 'NodeJS', 'Java'],
+	rightLists: ['TS', 'Python', 'C#'],
+};
