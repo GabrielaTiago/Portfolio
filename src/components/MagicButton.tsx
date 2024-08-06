1325c78db49484719792243b5d0d3af38eaae25d
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-06 10:50:47 -0300

    ✨ feat: Update MagicButton component to include optional 'type' prop

--- src/components/MagicButton.tsx
+++ src/components/MagicButton.tsx
@@ -4,17 +4,20 @@ export const MagicButton = ({
 	position,
 	handleClick,
 	otherClasses,
+	type,
 }: {
 	title: string;
 	icon: React.ReactNode;
 	position: string;
 	handleClick?: () => void;
 	otherClasses?: string;
+	type?: 'button' | 'submit' | 'reset';
 }) => {
 	return (
 		<button
-			className='relative inline-flex h-12 w-full mt-2 md:w-60 md:mt-6 overflow-hidden rounded-lg p-[1px] focus:outline-none'
+			className='relative inline-flex h-12 w-full mt-2 md:mt-6 overflow-hidden rounded-lg p-[1px] focus:outline-none'
 			onClick={handleClick}
+			type={type}
 		>
 			<span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
 
